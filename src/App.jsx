2133abf3a05d84eeed6fa97bcb359ac805d3eeb5
queuing4oranges <katjazenker@gmail.com>
2023-04-26 18:40:37 +0200

    jobinfo component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,46 @@
 import React, { useEffect, useState } from 'react';
+import Loading from './Loading';
+import JobInfo from './JobInfo';
 
 const url = 'https://course-api.com/react-tabs-project';
 
 const App = () => {
-  const [jobs, setJobs] = useState()
+  const [jobs, setJobs] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  useEffect(() => {
 
-  return <h2>Tabs Starter</h2>;
+    const fetchJobs = async() => {
+      try{
+        setIsLoading(true);
+        const response = await fetch(url);
+        const result = await response.json();
+        setJobs(result)
+        console.log(jobs)
+        setIsLoading(false)
+      } catch(error) {
+        console.log(error)
+      }
+    }
+    fetchJobs();
+  }, [])
+
+
+  return (
+    <div>
+      <h2>Full Stack Web Developer</h2>
+      {isLoading &&
+      <Loading />}
+      {jobs &&
+      jobs.map((singleJob, i) => (
+        <div key={i}>
+          <JobInfo singleJob={singleJob}/>
+        </div>
+      ))
+      }
+
+
+    </div>
+    )
+    
 };
 export default App;
---
+++ src/JobInfo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function JobInfo({ singleJob }) {
+
+ const { company, dates, title, duties } = singleJob
+ 
+console.log(company)
+
+  return (
+    <div></div>
+  )
+}
