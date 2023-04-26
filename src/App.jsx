594d78824c717610f6fd4d66ea1345c6014a0177
queuing4oranges <katjazenker@gmail.com>
2023-04-26 18:17:26 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from 'react';
+
+const url = 'https://course-api.com/react-tabs-project';
+
+const App = () => {
+  const [jobs, setJobs] = useState()
+
+  return <h2>Tabs Starter</h2>;
+};
+export default App;
